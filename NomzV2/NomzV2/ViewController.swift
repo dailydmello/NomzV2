698aebbbdfd7670b8782428570bc9ed67a3f9770
Ethan D'Mello <dailydmello@gmail.com>
2019-09-05 17:30:27 -0400

    Create xcode project

---
+++ NomzV2/NomzV2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NomzV2
+//
+//  Created by Ethan D'Mello on 2019-09-05.
+//  Copyright © 2019 Ethan D'Mello. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
